0976a3cae53016347d4efc5397f75a388a2642b5
Ahmed Akram <aelsemellawy@microsoft.com>
2025-05-31 14:06:41 +0300

    fixed vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,28 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-const apiUrl = import.meta.env.VITE_API_URL;
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: apiUrl,
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        crypto: 'crypto-js'
       }
     }
-  },
-  resolve: {
-    alias: {
-      crypto: 'crypto-js'
-    }
-  }
-}) 
+  };
+});
